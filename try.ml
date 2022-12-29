9198cdf8fdc6b0e5877357ee97733a7cb3674466
seunghyun-24 <kmasolo24@gmail.com>
2022-12-29 14:30:07 +0900

    ver 1

--- try.ml
+++ try.ml
@@ -1,31 +1,32 @@
-(*entry_of_channel : in_channel -> (int * int)
-dictionary_of_file : in_channel -> (int * int) list
-*)
-(*아 힘들다*)
+(*1차 ! : MUTAG_A.txt 읽어보기
+  명령어 : dictionary_of_file "MUTAG_A.txt";;
+  결과값 : string -> (int * int) list = <fun> *)
+
+let mdict = ref []
+let extend_dict (k) = mdict := k::!mdict
+
 let entry_of_channel : in_channel -> (int * int)
-(*let entry_of_channel ch =*)
 = fun ch ->
-  let e1 = input_line ch i다
+  let e1 = input_line ch in
   let e2 = input_line ch in
     (int_of_string e1, int_of_string e2)
 
 let rec dictionary_of_channel : in_channel -> (int * int) list
 = fun ch ->
-(*let rec dictionary_of_channel ch =  *)
   try
     let e = entry_of_channel ch in
-      e :: dictionary_of_channel ch
+      e::(dictionary_of_channel ch)
   with 
     End_of_file -> []
-
+  
 (*dictionary_of_file : string -> (int * int) list*)
 
-let dictionary_of_file filename = 
+let dictionary_of_file filename
+= fun filename -> 
   let ch = open_in filename in
   let dict = dictionary_of_channel ch in 
     close_in ch;
     dict
-
 
     (*
 dictionary_to_file : (int * int) -> (int * int) list -> unit
@@ -34,7 +35,6 @@ let dictionary_to_file filename graphEdge =
   let ch = open_out filename in 
     dictionary_to_file ch graphEdge;
     close_out ch
-
 
 let read_file filename =
   let lines = ref [] in
