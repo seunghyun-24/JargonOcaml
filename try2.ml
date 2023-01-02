8deadcb9580ba9210a2d3f98adea4fae9de8aa3b
seunghyun-24 <kmasolo24@gmail.com>
2023-01-02 19:32:41 +0900

    ADD

--- try2.ml
+++ try2.ml
@@ -2,11 +2,9 @@
 graph_to_nodes = {}
 node_to_graph = {}*)
 
-let graph_to_edges = {||}
-let graph_to_nodes = {||}
-let node_to_graph = {||}
-
-
+let graph_to_edges = Array.create_matrix
+let graph_to_nodes = Array.create_matrix
+let node_to_graph = Array.create_matrix
 
 (*with open("MUTAG/MUTAG_graph_indicator.txt") as file:
   i = 0 
@@ -21,27 +19,29 @@ let node_to_graph = {||}
     i = i+1 *)
 
 let read_graph_indicator oFile
-= let lines = ref 0 in
+= let i = ref 0 in
   try while true do
-    lines := lines + 1;
     let line = input_line oFile in
     let graph_idx = int_to_string line in
     let idx = graph_idx -1 in      
-    if (Array.mem idx graph_to_nodes = false) graph_to_nodes[idx]
-  
+    if (Array.mem idx graph_to_nodes = false) Array.append graph_to_nodes.(idx) [];
+    else ()
+    Array.append graph_to_nodes.(idx) i;
+    Array.append graph_to_nodes.(idx) [];
+    i := i + 1
   done
-
   with
-    End_of_file -> ();
+    End_of_file -> ()
     
-    let file_graph_indicator filename
-    = let oFile = open_in filename in
-      try
-        read_graph_indicator oFile;
-        close_in oFile;
-      with
-      _ -> close_in oFile
+let file_graph_indicator filename
+= let oFile = open_in filename in
+  try
+    read_graph_indicator oFile;
+    close_in oFile;
+  with
+  _ -> close_in oFile
 
+  (*
 A = []
 with open("MUTAG/MUTAG_A.txt") as file:
   i = 0
@@ -81,12 +81,16 @@ with open("MUTAG/MUTAG_node_labels.txt") as file:
       max_node_label = int_label
 
 
-X_node = []
-for i in range(len(node_to_label)):
-  X_node.append([])
-  for _ in range(max_node_label+1):
-    X_node[i].append(0)
+let x_node = ref [||]
 
+let make_x_node
+= for i = 0 to node_to_label.len do
+    x_node.append([]);
+    for j = 0 to max_node_label.len+1 
+    x_node.(i).append(0)
+done
 
-for _, val in enumerate(node_to_label):
-  X_node[val][node_to_label[val]] = 1
+let update_x_node 
+= for val = 0 to node_to_label.len do
+  x_node.(val).(node_to_label.(val)) = 1
+done
